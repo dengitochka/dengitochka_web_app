d61f6dcc3b32565e36f9d1bd62c166e77afa0a33
djino <malkinevgeniy437@gmail.com>
2023-01-13 01:36:42 +0300

    upd

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_Config = {
-    url: 'https://report-page.herokuapp.com/',
+    url: 'https://report-page.herokuapp.com',
     endpoints: {
         get_products: '/products/get',
         send_document: '/order/get'
